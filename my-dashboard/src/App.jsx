f6f30163289f0749dbfa8de5b797e1c05715baf0
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-11 14:27:21 +0000

    Fix import typo

--- my-dashboard/src/App.jsx
+++ my-dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import Header from '.Header';
+import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
